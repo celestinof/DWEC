e730d71714c4b58c0d78b596b4d0f8e955e76b73
celestinof <celestinofernandoit@gmail.com>
2025-10-17 13:23:38 +0200

    primer commit

---
+++ Unidad1/scripts/miScript.js
@@ -0,0 +1,12 @@
+
+
+// 1. cambio a console.log() para que se muestre en la terminal.
+console.log('Hola mundo'); 
+
+// 2. Llamo a la función
+const saludar = () => {
+    console.log('Mensaje desde la función saludar');
+}
+
+// 3. Llamar a la función para que se ejecute en Node
+saludar();
